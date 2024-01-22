479404c7f7100dbcd1d06f20ae2b155a75a48f83
Borgesfacundo <borgesfacundo@gmail.com>
2024-01-21 21:03:49 -0300

    asd

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -12,18 +12,18 @@ const profilePicture = "/images/placeholder.png";
 
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById("name");
-const foodElement = document.getElementById("food");
-const yearElement = document.querySelector("#year");
-const imageElement = document.querySelector("img");
+nameElement = document.getElementById("name");
+foodElement = document.getElementById("food");
+yearElement = document.querySelector("#year");
+imageElement = document.querySelector("img");
 
 
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
-imageElement.setAttribute("src" , profilePicture);
-imageElement.setAttribute("alt" , `profile image of ${fullName}`);
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `profile image of ${fullName}`);
 
 /* Step 5 - Array */
 foods = ["Milanesas Napolitanas", "Pizza", "Ñoquis", "Chicken Alfredo", "Marineras", "Feijoada", "Empanadas"];
