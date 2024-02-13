dfd393c8edc460057cd74a282ab30e90d1c8864a
Borgesfacundo <borgesfacundo@gmail.com>
2024-02-13 00:03:30 -0300

    Final Project

---
+++ Final Project/scripts/scripts.js
@@ -0,0 +1,75 @@
+/* Final Project */
+
+/* Declare global variables */
+const cardsElement = document.querySelector("#cards");
+let cardList = {};
+
+
+/* async displayCards Function 
+Add cards info to the cardsElement */
+const displayCards = (cards) => {
+    cards.forEach(card => {
+        const article = document.createElement("article");
+        const h3 = document.createElement("h3");
+        h3.textContent = card.name;
+        const picture = document.createElement("img");
+        picture.setAttribute("src", card.img);
+        picture.setAttribute("alt", card.name);
+        article.appendChild(h3);
+        article.appendChild(picture);
+        cardsElement.appendChild(article);
+    })
+};
+
+
+/* async getCards Function with fetch()
+Here i am getting the information abot cards via a JSON file*/
+const getCards = async () => {
+    const response = await fetch("json/Yu-Gi-OhCards.json");
+    if (response.ok) {
+        const data = await response.json();
+        templeList = data;
+        displayTemples(templeList);
+    }
+
+    console.log(templeList);
+}
+
+
+/* reset Function */
+const reset = () => {
+    var articles = templesElement.querySelectorAll('article');
+
+    articles.forEach(function (article) {
+        templesElement.removeChild(article);
+    })
+}
+
+/* filterTemples Function */
+filterTemples = (temples) => {
+    reset();
+    const filter = document.querySelector("#filtered").value;
+
+    switch (filter) {
+        case "utah":
+            displayTemples(temples.filter(temple => temple.location.includes("Utah")));
+            break;
+        case "notutah":
+            displayTemples(temples.filter(temple => !temple.location.includes("Utah")));
+            break;
+        case "older":
+            displayTemples(temples.filter(temple => new Date(temple.dedicated) < new Date(1950, 0, 1)));
+            break;
+        case "all":
+            displayTemples(temples)
+            break;
+    }
+};
+
+
+
+/* Event Listener */
+document.querySelector("#filtered").addEventListener("change", () => { filterTemples(templeList); });
+
+getTemples();
+
